81bf0e49c795fd41fcf980d77967276c9727f838
syamda17 <syafinaamanda335@gmail.com>
2024-09-27 00:42:47 +0700

    Add files via upload

---
+++ Tugas mandiri_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("          STT MANADALA          \n");
+	
+	printf("================================\n");
+	
+	printf("Nama      : SYAFINA AMANDA.\n");
+	
+	printf("Prodi     : TEKNIK INFORMATIKA.\n");
+	
+	printf("NIM       : 2441047.\n");
+	
+	return 0;
+}
---
+++ Tugas mandiri_2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int PN, LB, LS;
+	
+	PN=10;
+	
+	LB=7;
+	
+	
+	printf("Di ketahui panjang persegi 10CM.\n");
+
+    printf("Dan lebar persegi 7CM.\n");
+		
+	printf("Maka luas persegi adalah...?\n");
+	
+	printf("Panjang x Lebar = ");
+	
+	LS=PN*LB;
+	
+	printf("%d",LS);
+	
+	return 0;
+}
